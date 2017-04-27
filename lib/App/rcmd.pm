31572c04471fd362daed5e68c5fad49dbdb844c0
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-04-27 12:01:18 +0000

    Genesis

---
+++ lib/App/rcmd.pm
@@ -0,0 +1,12 @@
+# ABSTRACT: execute a (built-in) command on remote hosts over SSH
+use strict;
+use warnings;
+package App::rcmd;
+
+=head1 SYNOPSIS
+
+This is a stub module, see F<rcmd> script (F<bin/rcmd>) for details.
+
+=cut
+
+1;
